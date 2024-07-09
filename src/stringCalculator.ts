2b45d8d93665f398f82f0f1c4b0e20bc43e7cdbe
Isha <60545493+ishasinghh@users.noreply.github.com>
2024-07-09 18:31:36 +0530

    added test case and modified staringCalculator to handle diff conditions

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,7 @@
 export class StringCalculator {
   add(numbers: string): number {
-    return 0;
+    if (numbers === "") return 0;
+    const numArray = numbers.split(",");
+    return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
   }
 }
--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -10,4 +10,16 @@ describe("stringCalculator", () => {
   test("should return 0 for an empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
+
+  test("should return the number for a single number input", () => {
+    expect(calculator.add("1")).toBe(1);
+  });
+
+  test("should return the sum of two numbers", () => {
+    expect(calculator.add("1,5")).toBe(6);
+  });
+
+  test("should handle an unknown amount of numbers", () => {
+    expect(calculator.add("1,2,3,4,5")).toBe(15);
+  });
 });
