8770253629c05778a15c37bf1bbc20fdce039ff8
Isha <60545493+ishasinghh@users.noreply.github.com>
2024-07-09 19:48:47 +0530

    different delimeters handled

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,7 +1,17 @@
 export class StringCalculator {
   add(numbers: string): number {
     if (numbers === "") return 0;
-    const numArray = numbers.split(/[\n,]/);
+
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+      const delimiterMatch = numbers.match(/^\/\/(.)\n/);
+      if (delimiterMatch) {
+        delimiter = new RegExp(delimiterMatch[1]);
+        numbers = numbers.substring(delimiterMatch[0].length);
+      }
+    }
+
+    const numArray = numbers.split(delimiter);
     return numArray.reduce((sum, num) => sum + parseInt(num, 10), 0);
   }
 }
